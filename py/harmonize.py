7e9e45a7ac4783c53648268f39ad50dced2421fa
Arpan Laha <arpanlaha99@gmail.com>
2019-02-19 22:16:23 -0600

    split pitches into measures

--- py/harmonize.py
+++ py/harmonize.py
@@ -1,8 +1,35 @@
 from utils.analyzer import analyze
+from math import ceil
 
 
 def harmonize(filename, key=None, tempo=None, time_signature=None):
-    analyze(filename)
+    analysis = analyze(filename)
+
+    pitches = analysis["pitches"]
+
+    if key is None:
+        key = analysis["key"]
+
+    if tempo is None:
+        tempo = analysis["bpm"]
+
+    if time_signature is None:
+        time_signature = 4
+
+    measure_length = (
+        time_signature * (60 / tempo) * (44100 / 128)
+    )  # (beats/measure) (seconds/ebat) * (bins/second) = pitch bins/measure
+
+    num_measures = ceil(len(pitches) / measure_length)
+
+    measures = [
+        pitches[
+            int(i * measure_length) : int(max(len(pitches), (i + 1) * measure_length))
+        ]
+        for i in range(num_measures)
+    ]  # split pitches into measures
+
+    # time = len(pitches) / (44100 / 128)
 
 
 harmonize("simple.wav")
