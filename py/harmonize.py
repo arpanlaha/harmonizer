0d785d8925aa35f4ee5f18de543f07d37a9cc980
Arpan Laha <arpanlaha99@gmail.com>
2019-02-20 00:10:06 -0600

    change naming from pitch to frequency

--- py/harmonize.py
+++ py/harmonize.py
@@ -6,7 +6,7 @@ from math import ceil
 def harmonize(filename, key=None, tempo=None, time_signature=None):
     analysis = analyze(filename)
 
-    pitches = analysis["pitches"]
+    frequencies = analysis["frequencies"]
 
     if key is None:
         key = analysis["key"]
@@ -19,23 +19,25 @@ def harmonize(filename, key=None, tempo=None, time_signature=None):
 
     measure_length = (
         time_signature * (60 / tempo) * (44100 / 128)
-    )  # (beats/measure) (seconds/ebat) * (bins/second) = pitch bins/measure
+    )  # (beats/measure) (seconds/ebat) * (bins/second) = frequency bins/measure
 
-    num_measures = ceil(len(pitches) / measure_length)
+    num_measures = ceil(len(frequenci) / measure_length)
 
     measures = [
-        pitches[
-            int(i * measure_length) : int(max(len(pitches), (i + 1) * measure_length))
+        frequencies[
+            int(i * measure_length) : int(
+                max(len(frequencies), (i + 1) * measure_length)
+            )
         ]
         for i in range(num_measures)
-    ]  # split pitches into measures
+    ]  # split frequencies into measures
 
     chords = [""] * num_measures
     chords[0] = model["keys"][key]["chords"][0]
     chords[len(measures) - 1] = chords[0]
     print(chords)
 
-    # time = len(pitches) / (44100 / 128)
+    # time = len(frequencies) / (44100 / 128)
 
 
 harmonize("simple.wav")
