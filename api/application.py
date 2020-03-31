1d0ff4b03da3f4bd0aab948b5b6df86690175049
Arpan Laha <arpanlaha99@gmail.com>
2020-03-31 17:37:30 +0000

    try base app

--- api/application.py
+++ api/application.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, Response
-from flask_cors import CORS
 from views.harmony import harmony
 
 app = Flask(__name__)
-CORS(app)
-app.register_blueprint(harmony)
+# app.register_blueprint(harmony)
+@app.route("/")
+def hello():
+    return "Hello World!"
