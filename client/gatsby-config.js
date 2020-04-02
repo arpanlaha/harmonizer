e36ec497174ece320631dbf133dc57245fda2dc2
Arpan Laha <arpanlaha99@gmail.com>
2020-04-02 17:17:10 +0000

    preload fonts

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
         background_color: "#1a1e23",
         theme_color: "#1a1e23",
         display: "minimal-ui",
-        icon: "static/logo.svg"
-      }
+        icon: "static/logo.svg",
+      },
     },
     "gatsby-plugin-offline",
     "gatsby-plugin-react-helmet",
@@ -24,8 +24,8 @@ module.exports = {
       resolve: "gatsby-plugin-typescript",
       options: {
         allExtensions: true,
-        isTSX: true
-      }
-    }
-  ]
+        isTSX: true,
+      },
+    },
+  ],
 };
