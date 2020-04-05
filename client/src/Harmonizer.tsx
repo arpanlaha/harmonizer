3c65549af81562c18e20f7b1124ae03b86b7520b
Arpan Laha <arpanlaha99@gmail.com>
2020-04-05 18:00:52 +0000

    add comments

--- client/src/Harmonizer.tsx
+++ client/src/Harmonizer.tsx
@@ -28,7 +28,6 @@ export default function Harmonizer(): ReactElement {
   const [error, setError] = useState("");
   const [result, setResult] = useState<HarmonizeResult | null>(null);
   const [audioSource, setAudioSource] = useState(ctx.createBufferSource());
-
   const [melodyBuffer, setMelodyBuffer] = useState(
     ctx.createBuffer(1, 1, ctx.sampleRate)
   );
@@ -39,8 +38,11 @@ export default function Harmonizer(): ReactElement {
     ctx.createBuffer(1, 1, ctx.sampleRate)
   );
   const [playing, setPlaying] = useState(false);
-  const [time, setTime] = useState(0);
+  const [playTime, setPlayTime] = useState(0);
 
+  /**
+   * Set melody buffer on file upload
+   */
   useEffect((): void => {
     if (melodyFile !== null) {
       melodyFile
@@ -51,18 +53,25 @@ export default function Harmonizer(): ReactElement {
     }
   }, [melodyFile, setMelodyBuffer]);
 
+  /**
+   * Synthesize harmony and sets buffer on harmony parameter change
+   */
   useEffect((): void => {
     if (result !== null) {
       const { bpm, chords } = result;
       Transport.bpm.value = bpm;
       const measureLength = (60 * timeSignature) / bpm;
+
+      // synthesize audio and render into output buffer
       Offline((): void => {
+        // Ooe synth per chord note
         const synths = [
           new Synth().toDestination(),
           new Synth().toDestination(),
           new Synth().toDestination(),
         ];
 
+        // trigger 3 * chord progression length attack/releases
         chords.forEach((chord, chordIndex): void =>
           synths.forEach(
             (synth, synthIndex): FMSynth =>
@@ -74,6 +83,7 @@ export default function Harmonizer(): ReactElement {
           )
         );
       }, melodyBuffer.duration).then((buffer): void => {
+        // convert Tone.js type into Web Audio API type and set buffer
         const newBuffer = buffer.get();
         if (newBuffer !== undefined) {
           setHarmonyBuffer(newBuffer);
@@ -82,13 +92,20 @@ export default function Harmonizer(): ReactElement {
     }
   }, [result, melodyBuffer]);
 
+  /**
+   * Overlay melody and harmony buffers when either is changed
+   */
   useEffect((): void => {
+    // initialize new overlay buffer
     const newOverlayBuffer = ctx.createBuffer(
       melodyBuffer.numberOfChannels,
       melodyBuffer.length,
       ctx.sampleRate
     );
+    // harmony is mono
     const harmonyBufferChannel = harmonyBuffer.getChannelData(0);
+
+    // melody has arbitrary channels
     for (let channel = 0; channel < melodyBuffer.numberOfChannels; channel++) {
       const newOverlayBufferChannel = newOverlayBuffer.getChannelData(channel);
       const melodyBufferChannel = melodyBuffer.getChannelData(channel);
@@ -100,14 +117,24 @@ export default function Harmonizer(): ReactElement {
     setOverlayBuffer(newOverlayBuffer);
   }, [melodyBuffer, harmonyBuffer]);
 
+  /**
+   * Reinitialize audioSource with overlayBuffer
+   */
   const resetAudioSource = useCallback((): void => {
     const newAudioSource = ctx.createBufferSource();
     newAudioSource.buffer = overlayBuffer;
     setAudioSource(newAudioSource);
   }, [overlayBuffer, setAudioSource]);
 
+  /**
+   * Reset audioSource on chnange to overlayBuffer
+   */
   useEffect((): void => resetAudioSource(), [overlayBuffer, resetAudioSource]);
 
+  /**
+   * Reset application state prior to new upload
+   * @param file the melody file being uploaded
+   */
   const beforeUpload = (file: File): boolean => {
     setLoading(true);
     setError("");
@@ -117,62 +144,88 @@ export default function Harmonizer(): ReactElement {
     return true;
   };
 
-  const handleUpload = (e: UploadChangeParam): void => {
-    const { response, status } = e.file;
+  /**
+   * Update application state on upload change
+   * @param uploadChange the upload change object
+   */
+  const handleUpload = (uploadChange: UploadChangeParam): void => {
+    const { file, event } = uploadChange;
+    const { response, status } = file;
 
-    if (status === "uploading" && e.event !== undefined) {
-      setPercent(e.event.percent);
+    if (status === "uploading" && event !== undefined) {
+      // update progress bar
+      setPercent(event.percent);
     } else if (status === "done") {
+      // set result and end loading
       setResult(response.result);
       setLoading(false);
     } else if (status === "error") {
+      // set error and end loading
       setError(response?.message ?? "Unknown error");
       setLoading(false);
     }
   };
 
+  /**
+   * Start harmonized audio playback
+   */
   const handlePlay = useCallback((): void => {
     audioSource.connect(ctx.destination);
-    let newTime = time;
-    if (time >= melodyBuffer.duration - PLAYBACK_INTERVAL * 2) {
-      newTime = 0;
-      setTime(newTime);
+
+    // Fetch current time and reset to start if previous playback finished
+    let newPlayTime = playTime;
+    if (playTime >= melodyBuffer.duration - PLAYBACK_INTERVAL * 2) {
+      newPlayTime = 0;
+      setPlayTime(newPlayTime);
     }
 
-    audioSource.start(0, newTime);
+    audioSource.start(0, newPlayTime);
     setPlaying(true);
 
-    const startTime = ctx.currentTime;
-    const timer = setInterval(
-      (): void => setTime(ctx.currentTime - startTime + newTime),
+    // Start timer to update playTime every PLAYBACK_INTERVAL
+    const playStartTime = ctx.currentTime;
+    const playTimer = setInterval(
+      (): void => setPlayTime(ctx.currentTime - playStartTime + newPlayTime),
       PLAYBACK_INTERVAL * 1000
     );
 
+    // Clear timer and reset audioSource on stop
     audioSource.onended = (): void => {
-      clearInterval(timer);
+      clearInterval(playTimer);
       resetAudioSource();
       setPlaying(false);
     };
-  }, [audioSource, melodyBuffer, time, resetAudioSource]);
+  }, [audioSource, melodyBuffer, playTime, resetAudioSource]);
 
+  /**
+   * Stop harmonized audio playback
+   */
   const handleStop = useCallback((): void => {
     audioSource.stop();
     setPlaying(false);
   }, [audioSource]);
 
+  /**
+   * Update playTime from slider input
+   * @param newPlayTime slider input value
+   */
   const handleSlider = useCallback(
-    (value: SliderValue): void => {
-      if (typeof value !== "number") {
+    (newPlayTime: SliderValue): void => {
+      if (typeof newPlayTime !== "number") {
         return;
       }
       if (playing) {
         handleStop();
       }
-      setTime(value);
+      setPlayTime(newPlayTime);
     },
     [playing, handleStop]
   );
 
+  /**
+   * Convert time to standard mm:ss format
+   * @param seconds time in seconds
+   */
   const formatTime = (seconds: number): string => {
     let secondsLeft = seconds;
     const minutes = Math.floor(secondsLeft / 60);
@@ -228,7 +281,7 @@ export default function Harmonizer(): ReactElement {
             <h3>Chords: {result.chords.join(", ")}</h3>{" "}
             <div className="player">
               <h3 className="time">
-                {formatTime(time)} / {formatTime(melodyBuffer.duration)}
+                {formatTime(playTime)} / {formatTime(melodyBuffer.duration)}
               </h3>
               <Button
                 type="primary"
@@ -238,7 +291,7 @@ export default function Harmonizer(): ReactElement {
               </Button>
 
               <Slider
-                value={time}
+                value={playTime}
                 min={0}
                 max={melodyBuffer.duration}
                 step={PLAYBACK_INTERVAL}
