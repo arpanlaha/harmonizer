fb95604ebc82f19f5b904fb2a1cb62bcf344c4f8
Arpan Laha <arpanlaha99@gmail.com>
2020-04-14 23:45:12 +0000

    decay synths

--- client/src/utils/synth.ts
+++ client/src/utils/synth.ts
@@ -1,4 +1,4 @@
-import { FMSynth, Transport, Offline, ToneAudioBuffer } from "tone";
+import { FMSynth, Offline, ToneAudioBuffer, Transport } from "tone";
 import { HarmonyResult } from "./api";
 import { Chords } from "./theory";
 const Synth = FMSynth;
@@ -9,17 +9,21 @@ export const synthesizeHarmony = (
 ): Promise<ToneAudioBuffer> => {
   const { bpm, chords, meter, start } = result;
 
+  const createSynth = () =>
+    new Synth({
+      envelope: {
+        decay: 240 / bpm,
+        sustain: 0.5,
+      },
+    }).toDestination();
+
   Transport.bpm.value = bpm;
   const measureLength = Math.min((60 * meter) / bpm, duration);
 
   // synthesize audio and render into overlay buffer
   return Offline((): void => {
     // one synth per chord note
-    const synths = [
-      new Synth().toDestination(),
-      new Synth().toDestination(),
-      new Synth().toDestination(),
-    ];
+    const synths = [createSynth(), createSynth(), createSynth()];
 
     // decreasing order of volume: tonic, dominant, mediant
     const decibelDiff = 2;
