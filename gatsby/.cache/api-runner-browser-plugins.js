08468a60bada9a9ad51ea00c3db2fa91a63a7575
Arpan Laha <arpanlaha99@gmail.com>
2020-04-25 02:23:23 +0000

    remove lefotver files

--- gatsby/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"App name","short_name":"App short name","description":"App description","start_url":"/","background_color":"#ffffff","theme_color":"#ffffff","display":"browser","icon":"static/logo.svg"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
