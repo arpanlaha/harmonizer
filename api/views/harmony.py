237f89b0a562f6b489246f6e0c16b033f3359ae6
Arpan Laha <arpanlaha99@gmail.com>
2020-04-25 06:25:22 +0000

    cleanup harmony

--- api/views/harmony.py
+++ api/views/harmony.py
@@ -30,23 +30,23 @@ def allowed_file(filename):
 
 @harmony.route("", methods=["POST"])
 def harmonize():
-    file = request.files.get("melody")
+    melody = request.files.get("melody")
 
-    if file is None or file.filename == "":
+    if melody is None or melody.filename == "":
         return (
             jsonify({"success": False, "message": "No file provided"}),
             400,
         )
 
-    if not allowed_file(file.filename):
+    if not allowed_file(melody.filename):
         return jsonify({"success": False, "message": "Invalid file type"}), 400
 
     tempdir = TemporaryDirectory()
-    file_path = os.path.join(tempdir.name, file.filename)
-    file.save(file_path)
-    file.close()
+    melody_path = os.path.join(tempdir.name, melody.filename)
+    melody.save(melody_path)
+    melody.close()
 
-    audio = MonoLoader(filename=file_path)()
+    audio = MonoLoader(filename=melody_path)()
 
     frequencies, freq_confidence = PitchMelodia()(audio)  # binned list of frequencies
 
@@ -68,29 +68,11 @@ def harmonize():
     key = request.form.get("key", key + " " + scale)
     meter = float(request.form.get("meter", meter))
 
-    measure_length_bins = (
+    measure_bin_length = (
         meter * (60 / bpm) * (44100 / 128)
     )  # (beats/measure) (seconds/ebat) * (bins/second) = frequency bins/measure
 
-    num_measures = max(round(len(frequencies) / measure_length_bins), 1)
-
-    measures = [
-        frequencies[
-            int(i * measure_length_bins) : int(
-                min(len(frequencies), (i + 1) * measure_length_bins)
-            )
-        ]
-        for i in range(num_measures)
-    ]  # split frequencies into measures
-
-    confidences = [
-        freq_confidence[
-            int(i * measure_length_bins) : int(
-                min(len(frequencies), (i + 1) * measure_length_bins)
-            )
-        ]
-        for i in range(num_measures)
-    ]  # split frequencies into measures
+    num_measures = max(round(len(frequencies) / measure_bin_length), 1)
 
     # measure_lengths_seconds = [len(measure) / (44100 / 128) for measure in measures]
 
@@ -98,16 +80,66 @@ def harmonize():
 
     # assume first and last chords are tonic
     chords[0] = model["keys"][key]["chords"][0]
-    chords[len(measures) - 1] = chords[0]
+    chords[num_measures - 1] = chords[0]
 
     chords = request.form.get("chords", chords)
 
     for i in range(num_measures - 2):
         # start from second last and go backwards (important for contextual scoring)
         measure_number = len(chords) - 2 - i
-        chords[measure_number] = generate(
-            measures[measure_number], confidences[measure_number], key
-        )
+        measure_frequencies = frequencies[
+            int(measure_number * measure_bin_length) : int(
+                min(len(frequencies), (measure_number + 1) * measure_bin_length)
+            )
+        ]
+        measure_confidences = freq_confidence[
+            int(measure_number * measure_bin_length) : int(
+                min(len(frequencies), (measure_number + 1) * measure_bin_length)
+            )
+        ]
+
+        pitch_histogram = {}  # stores number of occurences of each pitch
+        index_histogram = {}  # stores number of occurences of each pitch
+
+        for i in range(len(measure_frequencies)):
+            frequency = measure_frequencies[i]
+            confidence = measure_confidences[i]
+            if frequency == 0.0:  # disregard invalid values
+                continue
+
+            A1 = 55
+            pitches = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+            pitch_index = round(12 * math.log2(frequency / A1)) % 12
+
+            pitch_name = pitches[pitch_index]
+
+            if pitch_name in pitch_histogram:
+                pitch_histogram[pitch_name] += confidence
+            else:
+                pitch_histogram[pitch_name] = confidence
+
+        chord_scores = {}  # arbitrarily defined scores for each possible chord
+        available_chords = model["keys"][key]["chords"]
+
+        for chord in available_chords:
+            chord_score = 0
+            notes = model["chords"][chord]["notes"]
+
+            for note in notes:
+                if (
+                    note not in pitch_histogram
+                ):  # disregard note if not detected in measure
+                    continue
+
+                if note == notes[0]:
+                    chord_score += 1.2 * pitch_histogram[note]
+                else:
+                    chord_score += pitch_histogram[note]
+
+            chord_scores[chord] = chord_score
+
+        chords[measure_number] = max(chord_scores.items(), key=itemgetter(1))[0]
 
     return (
         jsonify(
@@ -125,77 +157,3 @@ def harmonize():
         ),
         200,
     )
-
-
-def analyze(file):
-    audio = MonoLoader(filename=file)()
-
-    frequencies, freq_confidence = PitchMelodia()(audio)  # binned list of frequencies
-
-    key, scale, strength = KeyExtractor()(
-        audio
-    )  # key (note) and scale (major vs. minor) matter
-
-    if scale == "major":
-        scale = "Major"
-
-    bpm, beats = RhythmExtractor2013()(audio)[:2]  # tempo in beats per minute
-
-    loudness, loudness_band_ratio = BeatsLoudness(beats=beats)(audio)
-    beatogram = Beatogram()(loudness, loudness_band_ratio)
-    meter = Meter()(beatogram)
-
-    return {
-        "audio": audio,
-        "bpm": bpm,
-        "frequencies": frequencies,
-        "key": key + " " + scale,
-        "meter": meter,
-        "start": beats[0],
-    }
-
-
-def generate(frequencies, confidences, key, next=None):
-    pitch_histogram = {}  # stores number of occurences of each pitch
-    index_histogram = {}  # stores number of occurences of each pitch
-
-    for i in range(len(frequencies)):
-        frequency = frequencies[i]
-        confidence = confidences[i]
-        if frequency == 0.0:  # disregard invalid values
-            continue
-
-        A1 = 55
-        pitches = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-
-        pitch_index = round(12 * math.log2(frequency / A1)) % 12
-
-        pitch_name = pitches[pitch_index]
-
-        if pitch_name in pitch_histogram:
-            pitch_histogram[pitch_name] += confidence
-        else:
-            pitch_histogram[pitch_name] = confidence
-
-    chord_scores = {}  # arbitrarily defined scores for each possible chord
-    chords = model["keys"][key]["chords"]
-
-    for chord in chords:
-        chord_score = 0
-        notes = model["chords"][chord]["notes"]
-
-        for note in notes:
-            if note not in pitch_histogram:  # disregard note if not detected in measure
-                continue
-
-            if note == notes[0]:
-                chord_score += 1.2 * pitch_histogram[note]
-            else:
-                chord_score += pitch_histogram[note]
-
-        chord_scores[chord] = chord_score
-
-    """
-    To do: contextual scoring
-    """
-    return max(chord_scores.items(), key=itemgetter(1))[0]
